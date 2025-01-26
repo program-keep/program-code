4df7f881955843cc5637566ce29e2b8d56f9e184
program-keep <74966711+program-keep@users.noreply.github.com>
2025-01-26 23:29:28 +0800

    practice: 1.13 缺少垂直直方图打印

---
+++ c/practice/1.13.1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+#define SIZE 10
+/* print a histogram of the lengths of words in its input  vertical orientation*/
+/* 1 2 333 45678 999999999999999 */
+main(){
+	int c,i,state,lword;
+	int nwords[SIZE];
+	
+	state = OUT;
+	lword = 0;
+	
+	for(i =0;i<SIZE;++i){
+		nwords[i]=0;
+	}
+	
+	while((c=getchar())!=EOF){
+		if(c == ' '|| c== '\t'|| c=='\n'){
+			state = OUT;
+			if(lword!=0){
+				++nwords[lword];
+				lword = 0;
+			}
+		}else if(state == OUT){
+			state = IN;
+			++lword;
+		}else {
+			++lword;
+		}
+	}
+	
+	for(i =1;i<SIZE;++i){
+		printf("%d\t",nwords[i]);
+	}
+	printf("\n");
+	for(i =1;i<SIZE;++i){
+		printf("%d\t",i);
+	}
+	
+}
